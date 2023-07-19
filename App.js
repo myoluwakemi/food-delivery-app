0701f9f6f8966ecf41340341dac81726eff77351
oluwakemi <yusufoluwakemi39@gmail.com>
2023-07-19 14:40:10 +0100

    added expo package for custom fonts

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from "expo-font"
 
 export default function App() {
+  // useFonts({
+
+  // })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
