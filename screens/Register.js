720798bdf68eb464755cd1b38232b0361850c111
oluwakemi <yusufoluwakemi39@gmail.com>
2023-07-25 23:40:03 +0100

    created input field layout

--- screens/Register.js
+++ screens/Register.js
@@ -1,27 +1,68 @@
 import { useLayoutEffect } from "react"
-import { Button, Text, View } from "react-native"
+import { Text, View, StyleSheet, TextInput } from "react-native"
 import Ionicons from '@expo/vector-icons/Ionicons'
 import IconButton from "../components/IconButton"
+import PrimaryButton from "../components/PrimaryButton"
 
 
-const Register = ({navigation}) => {
+const Register = ({ navigation }) => {
 
     const handlePressedButton = () => {
         console.log('tapped!!')
     }
-    useLayoutEffect(()=> {
+    useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => {
-                return(<IconButton name='menu-outline' color='#000' size={18}/>)
+                return (<IconButton name='menu-outline' color='#000' size={18} />)
             }
         })
 
     }, [])
     return (
-        <View>
-            <Text>hihi</Text>
-           
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <Ionicons name="person-outline" size={18} color='#000' />
+                <TextInput />
+
+            </View>
+            <View style={styles.inputContainer}>
+                <Ionicons name="mail-outline" size={18} color='#000' />
+                <TextInput />
+
+            </View>
+              <View style={styles.inputContainer}>
+                <Ionicons name="lock-closed-outline" size={18} color='#000' />
+                <TextInput />
+
+            </View>
+            <PrimaryButton buttnText='Create Account'/>
+
         </View>
+
     )
 }
 export default Register
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 18,
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: 100
+       
+    },
+    inputContainer: {
+        borderColor:'rgba(0, 0, 0, 0.3)',
+        borderWidth: 1,
+        borderRadius: 12,
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+       paddingHorizontal: 18,
+       paddingVertical: 18,
+       width: '100%',
+       marginBottom: 14
+
+    }
+})
+
